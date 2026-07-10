8abbc1cb19e0fbb808b08ba7ab1df2bd92e611b7
kjanuda <janudakodi@gmail.com>
2026-07-10 21:18:08 +0530

    fix chromium file tracing

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["playwright-core", "@sparticuz/chromium"],
+  outputFileTracingIncludes: {
+    "/api/ceb-bill": [
+      "./node_modules/playwright-core/**",
+      "./node_modules/@sparticuz/chromium/**",
+    ],
+  },
 };
 
 export default nextConfig;
